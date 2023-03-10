4a963c3d383939f8927baf18b462fc2633e5b4d1
juperameza <juperameza@hotmail.com>
2023-03-10 10:02:18 -0600

    Added production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "login_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost',port: 3000, protocol: 'htpp'}
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.credentials.email[:username],
+    password:       Rails.application.credentials.email[:password],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true,
+    domain: 'gmail.com'
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
