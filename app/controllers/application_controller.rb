faea4a0a88ea5f505c42994253844c77b32969d8
juperameza <juperameza@hotmail.com>
2023-05-25 18:59:20 -0600

    Continue working on the administrator options
    Creation of a devise user while being an admin is done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday, :phone_number, :admin])
+      devise_parameter_sanitizer.permit(:create, keys: [:name, :birthday, :phone_number, :admin])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birthday, :phone_number, :admin])
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,30 @@ class UsersController < ApplicationController
         end
       end
 
+      def new
+        @user = User.new
+      end
+
+      def create
+        puts(user_params)
+        @user = User.create!(user_params)
+
+        respond_to do |format|
+          if @user.save
+            format.html { redirect_to  user_path, notice: "user was successfully created." }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+          end
+        end
+      end
       private
         def user_params
-            params.require(:user).permit(:name, :birthday, :phone_number,  :email)
+            params.require(:user).permit(:email, :name, :birthday,  :password, :password_confirmation, :admin)
             end
-            def admin_only
+
+          def admin_only
                 unless current_user.admin?
                   redirect_to root_path, :alert => "Access denied."
                 end
-              end
+          end
 end
